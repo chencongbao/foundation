ff6cbe16f7a7177eedbadd5adad53fa1169645d4
chencongbao <lhaiccb@126.com>
2026-02-23 10:54:53 +0800

    12
    12

--- src/Functions/helpers.php
+++ src/Functions/helpers.php
@@ -9,3 +9,25 @@ if (!function_exists('bob_admin_setting')) {
         return App::make(CacheAdminSettingService::class)->excute($name,$value);
     }
 }
+
+if (!function_exists('bob_ip')) {
+    function bob_ip($debug = false)
+    {
+        $data = [];
+        $ip = Request::ip();
+        $data['ip'] = $ip;
+        if (isset($_SERVER["HTTP_ALI_CDN_REAL_IP"]) && !empty($_SERVER["HTTP_ALI_CDN_REAL_IP"])) {
+            $ip = $_SERVER["HTTP_ALI_CDN_REAL_IP"];
+            $data['HTTP_ALI_CDN_REAL_IP'] = $ip;
+        }
+        if (isset($_SERVER["HTTP_CF_CONNECTING_IP"]) && !empty($_SERVER["HTTP_CF_CONNECTING_IP"])) {
+            $ip = $_SERVER["HTTP_CF_CONNECTING_IP"];
+            $data['HTTP_CF_CONNECTING_IP'] = $ip;
+        }
+        if (isset($_SERVER['HTTP_CLIENT_IP'])) {
+            $ip = $_SERVER['HTTP_CLIENT_IP'];
+            $data['HTTP_CLIENT_IP'] = $ip;
+        }
+        return $ip;
+    }
+}
